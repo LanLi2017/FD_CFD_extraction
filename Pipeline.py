6e8824e6182750663d0eae018f3267de1eb3f835
lan <lilan.scut@gmail.com>
2022-02-26 17:10:46 -0600

    test pipeline 1 with dataset 50

--- Pipeline.py
+++ Pipeline.py
@@ -5,34 +5,40 @@ from tane_cp import main as tane
 
 from utils.evaluation import evaluate_FDs
 
+
 def tune_parameters():
     pass
 
 
-def run():
+def run(clean_data_path='Database/exp_data/employee_50_egtask_clean.csv',
+             dirty_data_path='Database/exp_data/employee_50_egtask_dirty.csv',
+             func='tane', k=20):
     # 1.Load clean dataset D0
     # 2.Use BART to generate dirty version dataset
     # 2.1 tune the parameters
     # 3. Use TANE/CTANE to generate rules based on D0 and dirty version dataset from 2
     # 4. do qualitative analysis
-    clean_data_path = 'Database/exp_data/employee_50_egtask_clean.csv'
-    dirty_data_path = 'Database/exp_data/employee_50_egtask_dirty.csv' # add 4 dirty cells [e1]
-    use_func = ['TANE', 'CTANE']
-    list_of_fds_dirty = tane(dirty_data_path)  # TANE with messy testdata
-    list_of_fds_gt = tane(clean_data_path) # TANE with clean testdata
+    list_of_fds_dirty = []
+    list_of_fds_gt = []
+    list_of_cfds_dirty = []
+    list_of_cfds_gt = []
 
-    print("dirty FDs: " + str(list_of_fds_dirty))
-    print("ground truth FDs: " + str(list_of_fds_gt))
+    if func == 'tane':
+        list_of_fds_dirty = tane(dirty_data_path)  # TANE with messy testdata
+        list_of_fds_gt = tane(clean_data_path)  # TANE with clean testdata
+        print("dirty FDs: " + str(list_of_fds_dirty))
+        print("ground truth FDs: " + str(list_of_fds_gt))
 
-    accuracy, precison, recall, f1 = evaluate_FDs(list_of_fds_gt, list_of_fds_dirty)
-    print("-------------------------")
-    print("evaluating tane FDs...")
-    print("-------------------------")
-    print("accuracy: %.3f\nprecison: %.3f\nrecall: %.3f\nf1: %.3f"% \
-        (accuracy, precison, recall, f1))
-    print("-------------------------")
-
-    # ctane(dirty_data_path, k=2)
+        accuracy, precison, recall, f1 = evaluate_FDs(list_of_fds_gt, list_of_fds_dirty)
+        print("-------------------------")
+        print("evaluating tane FDs...")
+        print("-------------------------")
+        print("accuracy: %.3f\nprecison: %.3f\nrecall: %.3f\nf1: %.3f" % \
+              (accuracy, precison, recall, f1))
+        print("-------------------------")
+    elif func == 'ctane':
+        list_of_cfds_dirty = ctane(dirty_data_path, k)  # TANE with messy testdata
+        list_of_cfds_gt = ctane(clean_data_path, k)  # TANE with clean testdata
 
 
 if __name__ == '__main__':
